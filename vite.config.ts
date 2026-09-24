4284f64c3440c60ab6c226233d4d41990c4981a2
Pavel Martynov <25pashok@mail.ru>
2026-09-24 15:43:09 +0500

    chore: инициализация проекта и зависимостей

---
+++ vite.config.ts
@@ -0,0 +1,33 @@
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
+import path from "path";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "@/api/*": path.resolve(__dirname, "./src/api/*"),
+      "@/assets/*": path.resolve(__dirname, "./src/assets/*"),
+      "@/components/*": path.resolve(__dirname, "./src/components/*"),
+      "@/constants/*": path.resolve(__dirname, "./src/constants/*"),
+      "@/features/*": path.resolve(__dirname, "./src/features/*"),
+      "@/hooks/*": path.resolve(__dirname, "./src/hooks/*"),
+      "@/lib/*": path.resolve(__dirname, "./src/lib/*"),
+      "@/pages/*": path.resolve(__dirname, "./src/pages/*"),
+      "@/store/*": path.resolve(__dirname, "./src/store/*"),
+      "@/types/*": path.resolve(__dirname, "./src/types/*"),
+    },
+  },
+  server: {
+    open: true,
+    proxy: {
+      "/api/v1": {
+        target: "http://127.0.0.1:8000",
+        changeOrigin: true,
+      },
+    },
+  },
+});
